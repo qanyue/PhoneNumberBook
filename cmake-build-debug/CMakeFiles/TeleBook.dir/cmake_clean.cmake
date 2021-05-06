2d5e7dbb1f8a2299b9f7658792930daefe1e96ed
qanyue <a1791661482@gmail.com>
2021-05-06 21:36:17 +0800

    Init

---
+++ cmake-build-debug/CMakeFiles/TeleBook.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TeleBook.dir/Source.cpp.obj"
+  "CMakeFiles/TeleBook.dir/main.cpp.obj"
+  "TeleBook.exe"
+  "TeleBook.exe.manifest"
+  "TeleBook.pdb"
+  "libTeleBook.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TeleBook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
